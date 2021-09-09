0fc9a71dd6054e4c26e16e742306a297ffaa4778
Behnam Mahdavirad <behnam.mahdavirad@gmail.com>
2021-09-09 18:36:26 +0430

    Add SubSytem3

---
+++ Facade/SubSystem3/Class3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade.SubSystem3
+{
+    public class Class3
+    {
+        public void Action3()
+        {
+            Console.WriteLine("Run ... SubSytem3.Class3.Action3");
+        }
+    }
+}
